34fcddff3b65a78cf2174f89acdaaac68cbb16ad
Olha75 <137830839+Olha75@users.noreply.github.com>
2024-02-06 17:50:30 +0200

    NavLink

--- src/components/MainMenu/MainMenu.jsx
+++ src/components/MainMenu/MainMenu.jsx
@@ -1,33 +1,33 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import css from './mainMenu.module.css';
 
 const MainMenu = () => {
   return (
     <ul className={css.menu}>
       <li>
-        <Link className={css.link} to="/">
+        <NavLink className={css.link} to="/">
           Home
-        </Link>
+        </NavLink>
       </li>
       <li>
-        <Link className={css.link} to="/movies">
+        <NavLink className={css.link} to="/movies">
           Movies
-        </Link>
+        </NavLink>
       </li>
       <li>
-        <Link className={css.link} to="/movies/:movieId">
+        <NavLink className={css.link} to="/movies/:movieId">
           MovieDetails
-        </Link>
+        </NavLink>
       </li>
       <li>
-        <Link className={css.link} to="/movies/:movieId/cast">
+        <NavLink className={css.link} to="/movies/:movieId/cast">
           Cast
-        </Link>
+        </NavLink>
       </li>
       <li>
-        <Link className={css.link} to="/movies/:movieId/reviews">
+        <NavLink className={css.link} to="/movies/:movieId/reviews">
           Reviews
-        </Link>
+        </NavLink>
       </li>
     </ul>
   );
