8055671d163b19b317d786173760c8e8a0fe61e7
Olha75 <137830839+Olha75@users.noreply.github.com>
2024-02-12 01:19:19 +0200

    css_cast_li

--- src/components/MovieDetails/Cast/Cast.jsx
+++ src/components/MovieDetails/Cast/Cast.jsx
@@ -2,8 +2,8 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getCastById } from '../../../api/api';
 import Loader from '../../Loader/Loader';
-import myImage from '../../../image/default_no_actor.jpg';
-// import css from './cast.module.css';
+import myImage from '../../../image/default_no_actor_1.jpg';
+import css from './cast.module.css';
 
 const Cast = () => {
   const [cast, setCast] = useState([]);
@@ -27,8 +27,9 @@ const Cast = () => {
   }, [movieId]);
 
   const elements = cast.map(({ id, original_name, profile_path }) => (
-    <li key={id}>
+    <li className={css.cast_items} key={id}>
       <img
+        className={css.img_cast}
         src={
           profile_path
             ? `https://image.tmdb.org/t/p/original${profile_path}`
@@ -46,7 +47,7 @@ const Cast = () => {
     <>
       {loading && <Loader />}
       {error && <p>{error}</p>}
-      {isCast && <ol>{elements}</ol>}
+      {isCast && <ul className={css.cast_list}>{elements}</ul>}
     </>
   );
 };
